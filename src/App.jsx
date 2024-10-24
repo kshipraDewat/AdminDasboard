4a409b3d9a5a5bb1e8757ff35c15dc312e265959
kshipraDewat <kshipradewat66@gmail.com>
2024-10-24 13:36:27 +0530

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,29 @@ import SalesPage from './pages/SalesPage'
 import OrdersPage from './pages/OrdersPage'
 import AnalyticsPages from './pages/AnalyticsPages'
 import Settings from './pages/Settings'
-import NavBar from './components/NavBar'
+import SideBar from './components/SideBar'
+
 
 function App() {
   return (
-    <div>
-    <NavBar/>
-     <Routes>
-        <Route path='/' element={<OverviewPage/>}/>
-        <Route path='/products' element={<ProductsPage/>}/>
-        <Route path='/users' element={<UsersPage/>}/>
-        <Route path='/sales' element={<SalesPage/>}/>
-        <Route path='/orders' element={<OrdersPage/>}/>
-        <Route path='/analytics' element={<AnalyticsPages/>}/>
-        <Route path='/settings' element={<Settings/>}/>
-      </Routes>
-    </div> 
+    <div className=' flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+    <div className='fixed inset-0 z-0'>
+      <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80'/>
+      <div className='absolute inset-0 backdrop-blur-sm'/>
+
+    </div>
+    <SideBar/>
+
+    <Routes>
+      <Route path='/' element={<OverviewPage/>}/>
+      <Route path='/products' element={<ProductsPage/>}/>
+      <Route path='/users' element={<UsersPage/>}/>
+      <Route path='/sales' element={<SalesPage/>}/>
+      <Route path='/orders' element={<OrdersPage/>}/>
+      <Route path='/analytics' element={<AnalyticsPages/>}/>
+      <Route path='/settings' element={<Settings/>}/>
+    </Routes>
+  </div>
   )
 }
 
