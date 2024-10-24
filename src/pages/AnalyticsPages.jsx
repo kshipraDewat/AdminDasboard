7ebcbb254057db0b6fb7ef312f007b6d472b7709
kshipraDewat <kshipradewat66@gmail.com>
2024-10-24 13:40:55 +0530

    Analytics page

--- src/pages/AnalyticsPages.jsx
+++ src/pages/AnalyticsPages.jsx
@@ -1,8 +1,32 @@
-
+import NavBar from '../components/NavBar'
+import OverviewCard from '../components/analytics/OverviewCard'
+import RevenueVsTargetChart from '../components/analytics/RevenueVsTargetChart'
+import ChannelPerformance from '../components/analytics/ChannelPerformance'
+import ProductPerformance from '../components/analytics/ProductPerformance'
+import UserRetention from '../components/analytics/UserRetention'
+import AIPoweredInsight from '../components/analytics/AIPoweredInsight'
+import CustSegChart from '../components/analytics/CustSegChart'
 
 const AnalyticsPages = () => {
   return (
-   <div>
+    <div className='flex-1 overflow-auto relative z-10'>
+      <NavBar title="Analytics Dashboard" />
+
+      <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+        <OverviewCard />
+        <RevenueVsTargetChart />
+
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
+          <ChannelPerformance />
+          <ProductPerformance />
+          <UserRetention />
+          <CustSegChart/>
+          
+        </div>
+        <AIPoweredInsight/>
+
+
+      </main>
 
     </div>
   )
